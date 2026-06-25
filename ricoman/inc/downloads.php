3ff058673fafc3cbf2c37efbe47ba9626f76f32d
Patricia Walton <pwalton@ricoman.com>
2026-06-25 13:32:39 +0100

    Downloads: force page-downloads template via PHP filter for full-width layout

--- ricoman/inc/downloads.php
+++ ricoman/inc/downloads.php
@@ -191,6 +191,17 @@ function ricoman_downloads_admin_page() {
 
 /* ------------------------------------------------------------------ shortcode / front-end */
 
+// Force the downloads page to use the full-width page-downloads template.
+add_filter( 'template_include', function( $template ) {
+	if ( is_page( 'downloads' ) && ! is_admin() ) {
+		$candidate = get_theme_file_path( 'templates/page-downloads.html' );
+		if ( file_exists( $candidate ) ) {
+			return $candidate;
+		}
+	}
+	return $template;
+} );
+
 add_shortcode( 'ricoman_downloads', 'ricoman_downloads_shortcode' );
 
 function ricoman_downloads_shortcode() {
