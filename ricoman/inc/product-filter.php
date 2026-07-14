bae8fd3d4a19a1d66038bd0cec7afb3911e3568d
Patricia Walton <pwalton@ricoman.com>
2026-07-14 14:32:24 +0100

    Fix category page grid: hide wpautop p tags inside product grid

--- ricoman/inc/product-filter.php
+++ ricoman/inc/product-filter.php
@@ -430,6 +430,7 @@ add_shortcode( 'ricoman_cat_filter', function ( $atts ) {
 		. '@media(max-width:1100px){.rm-allprods .rm-prodgrid,.rm-allprods .rm-allpgrid{grid-template-columns:repeat(3,1fr)}}'
 		. '@media(max-width:600px){.rm-allprods .rm-prodgrid,.rm-allprods .rm-allpgrid{grid-template-columns:1fr 1fr}}'
 		. '@media(max-width:400px){.rm-allprods .rm-prodgrid,.rm-allprods .rm-allpgrid{grid-template-columns:1fr}}'
+		. '.rm-allpgrid>p,.rm-allpgrid>br{display:none!important}'
 		. '[style*="display: none"]{display:none!important}'
 		. '</style>';
 	$out .= '<div class="rm-pp-wrap rm-catarch rm-allprods">';
